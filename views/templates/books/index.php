47606a11f74df14285025c45e07e400b853a3117
Neerz99 <iloenzo99@gmail.com>
2025-05-04 23:51:02 +0200

    added books page img css

--- views/templates/books/index.php
+++ views/templates/books/index.php
@@ -4,10 +4,11 @@
     <?php foreach ($books as $b): ?>
         <li class="book-list-item">
             <a href="/TomTroc/books/detail/<?= $b['id'] ?>">
-                <img src="<?= htmlspecialchars($b['imageUrl']) ?>" alt="<?= htmlspecialchars($b['title']) ?>">
+                <img class="book-list-item-image" src="<?= htmlspecialchars($b['imageUrl']) ?>" alt="<?= htmlspecialchars($b['title']) ?>">
             </a>
-            <?= htmlspecialchars($b['title']) ?> par <?= htmlspecialchars($b['author']) ?>
-            <p>Vendu par : <a href="<?= Utils::url('member', 'detail', [(int)$b['ownerId']]) ?>"><?=htmlspecialchars($b['ownerName']) ?></a></p>
+            <p class="book-list-item-title"><?= htmlspecialchars($b['title']) ?></p>
+            <p class="book-list-item-author"><?= nl2br(htmlspecialchars($b['author'])) ?></p>
+            <p class="book-list-item-owner"><em>Vendu par : <a href="<?= Utils::url('member', 'detail', [(int)$b['ownerId']]) ?>"><?=htmlspecialchars($b['ownerName']) ?></a></em></p>
         </li>
     <?php endforeach; ?>
 </ul>
