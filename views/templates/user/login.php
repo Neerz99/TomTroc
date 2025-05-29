e499f16c056936c9e4e120aab19820348bd04b5b
Neerz99 <iloenzo99@gmail.com>
2025-05-29 21:03:16 +0200

    added login/register CSS

--- views/templates/user/login.php
+++ views/templates/user/login.php
@@ -1,19 +1,32 @@
-<h1><?= htmlspecialchars($title) ?></h1>
+<div class="login-container">
 
-<?php if (!empty($error)): ?>
-    <p class="error"><?= htmlspecialchars($error) ?></p>
-<?php endif; ?>
+    <?php if (!empty($error)): ?>
+        <p class="error"><?= htmlspecialchars($error) ?></p>
+    <?php endif; ?>
 
-<form method="post" action="/TomTroc/user/login">
-    <div>
-        <label>Email :</label>
-        <input type="email" name="email" required>
+    <div class="login-content">
+
+        <form method="post" action="<?= Utils::url('user', 'login') ?>">
+
+            <h1><?= htmlspecialchars($title) ?></h1>
+
+            <div class="login-field">
+                <label>Email :</label>
+                <input type="email" name="email" required>
+            </div>
+            <div class="login-field">
+                <label>Mot de passe :</label>
+                <input type="password" name="password" required>
+            </div>
+
+            <button class="login-submit" type="submit">Se connecter</button>
+
+            <a class="login-register" href="<?= Utils::url('user', 'register') ?>">Pas encore inscrit ?</a>
+
+        </form>
+
+        <img class="login-image" src="../assets/images/login_page.jpg" alt="Librairie">
+
     </div>
-    <div>
-        <label>Mot de passe :</label>
-        <input type="password" name="password" required>
-    </div>
-    <button type="submit">Se connecter</button>
-</form>
 
-<p><a href="/TomTroc/user/register">Pas encore inscrit ?</a></p>
+</div>
